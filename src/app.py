ffc6c5f3f1ba76897cf560e7b312fa7ba3f7013b
Tommy Lees <thomas.lees112@gmail.com>
2024-07-04 15:18:47 +0100

    add async

--- src/app.py
+++ src/app.py
@@ -8,6 +8,11 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 from src.summariser import summarise_webpage
 from src.utils import format_elapsed_time
+from src.twilio_utils import (
+    get_or_create_conversation,
+    add_participant_to_client,
+    send_message_to_conversation,
+)
 from twilio.rest import Client
 from celery import Celery
 
@@ -20,13 +25,8 @@ logging.basicConfig(
     format="%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s",
 )
 
-# Setup API keys
-twilio_account_sid = os.getenv("TWILIO_ACCOUNT_SID")
-twilio_auth_token = os.getenv("TWILIO_AUTH_TOKEN")
-twilio_phone_number = os.getenv("TWILIO_PHONE_NUMBER")
-
-# Twilio REST API client
-client = Client(twilio_account_sid, twilio_auth_token)
+# load the keys in .env
+load_dotenv()
 
 # Celery instance
 redis_url = os.getenv(
@@ -52,7 +52,7 @@ def homepage():
 def webhook() -> str:
     # get the incoming message
     incoming_msg = request.values.get("Body", "").strip()
-    from_number = request.values.get("From", "")
+    from_number = request.values.get("From", "").replace("whatsapp:", "")
     logging.debug(f"\nNEW MESSAGE\nRecieved message [{from_number}]: {incoming_msg}")
 
     # Start the asynchronous task
@@ -63,39 +63,68 @@ def webhook() -> str:
 
 
 @celery.task
-def process_message_async(incoming_msg: str, from_number: str) -> str:
+def process_message_async(
+    incoming_msg: str,
+    from_number: str,
+) -> str:
+
+    # Setup API keys
+    twilio_account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+    twilio_auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+    twilio_phone_number = os.getenv("TWILIO_PHONE_NUMBER")
+
+    # Twilio REST API client
+    client = Client(twilio_account_sid, twilio_auth_token)
+
+    logging.debug(
+        f"process_message_async(\nfrom_number={from_number},\nincoming_msg={incoming_msg}\n)\nclient == {client}"
+    )
+
     # initialise timer
     start_time = time.time()
+
+    # initialise conversation
+    conversation_sid, client = get_or_create_conversation(client=client)
+    print(conversation_sid, client)
+    client = add_participant_to_client(
+        client=client,
+        conversation_sid=conversation_sid,
+        participant_phone=from_number,
+        twilio_phone_number=twilio_phone_number,
+    )
 
     if (
         (incoming_msg.startswith("@summarise"))
         or (incoming_msg.startswith("@summarize"))
         or (incoming_msg.startswith("@summ"))
     ):
-        # initialize the messages
-        # response = MessagingResponse()
-        # message = response.message()
-
         try:
             summary = summarise_webpage(incoming_msg)
             logging.debug(f"Summary of webpage:\n{summary}")
 
-            # create TwilioMessage via `message.body` attribute of the `response` object
-            # message.body(summary)
             # Send the summary via Twilio API
-            client.messages.create(
-                body=summary, from_=twilio_phone_number, to=from_number
+            client = send_message_to_conversation(
+                client, conversation_sid=conversation_sid, message=summary
             )
+            # client.messages.create(
+            #     body=summary,
+            #     from_=twilio_phone_number,
+            #     to=from_number,
+            # )
 
             logging.debug(f"Message sent to twilio: `message.body(summary)`")
 
         except Exception as e:
             logging.error(f"Error: {e}")
-            # message.body("An error occured while summarising the webpage.")
-            client.messages.create(
-                body="An error occured while summarising the webpage.",
-                from_=twilio_phone_number,
-                to=from_number,
+            # client.messages.create(
+            #     body="An error occured while summarising the webpage.",
+            #     from_=twilio_phone_number,
+            #     to=from_number,
+            # )
+            client = send_message_to_conversation(
+                client,
+                conversation_sid=conversation_sid,
+                message="An error occured while summarising the webpage.",
             )
 
         # end the timer
@@ -104,11 +133,11 @@ def process_message_async(incoming_msg: str, from_number: str) -> str:
         human_readable_time = format_elapsed_time(elapsed_time)
         logging.debug(f"Function finished: {human_readable_time}")
 
-        # Check the response
-        response = client.messages.body
-        logging.debug(f"Return MessagingResponse: \n`str({response})`")
-
-        return str(response)
+        # # Check the response
+        # response = client.messages.body
+        # logging.debug(f"Return MessagingResponse: \n`str({response})`")
+        # return str(response)
+        return "Success!"
     else:
         logging.warning(f"Recieved an invalid command: {incoming_msg}")
         return "Not called"
@@ -124,9 +153,6 @@ def favicon():
 
 
 if __name__ == "__main__":
-    # load the keys in .env
-    load_dotenv()
-
     # run the app
     logging.info("Starting the Flask app...")
     app.run(debug=True)

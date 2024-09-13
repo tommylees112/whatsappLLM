789ef2e769ca3f7d32cf15361607e2cb4fa54696
Tommy Lees <thomas.lees112@gmail.com>
2024-09-13 15:10:24 -0400

    udate

--- src/config.py
+++ src/config.py
@@ -34,7 +34,12 @@ class Settings(BaseSettings):
 
 
 def get_settings() -> Settings:
-    return Settings()
+    return Settings(
+        TWILIO_ACCOUNT_SID=os.environ.get("TWILIO_ACCOUNT_SID"),
+        TWILIO_AUTH_TOKEN=os.environ.get("TWILIO_AUTH_TOKEN"),
+        TWILIO_PHONE_NUMBER=os.environ.get("TWILIO_PHONE_NUMBER"),
+        COHERE_API_KEY=os.environ.get("COHERE_API_KEY"),
+    )
 
 
 # Create a global instance of the settings
